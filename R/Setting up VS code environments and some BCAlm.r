43a712d724536329e301bce57b8299d1c65eaf18
chanodya19 <chanodya2009@gmail.com>
2025-05-21 15:06:00 +0930

    add BCalm setup R script

---
+++ R/Setting up VS code environments and some BCAlm.r
@@ -0,0 +1,17 @@
+remotes::install_github("nx10/httpgd")#install necessary extensions for R in VS code
+install.packages("languageserver")
+install.packages("rmarkdown")
+
+
+# install released version
+pipx install radian # already done in /mnt/c/Users/chano/Bioinformatics/250516_BCalm_tutorial$ pathway
+
+# to run radian
+radian
+
+#load tidyverse
+library(tidyverse)
+install.packages("devtools") #install devtools
+library(devtools) #load from github using devtools
+
+
